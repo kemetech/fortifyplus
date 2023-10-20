0426686114f27515795d55d4ad6fc7a8f5ad3bdf
Moataz Elsayed <dr.moatazelsayed@gmail.com>
2023-10-20 08:06:29 +0300

    update admin migration

--- database/migrations/2023_10_18_233537_create_admins_table.php
+++ database/migrations/2023_10_18_233537_create_admins_table.php
@@ -3,9 +3,10 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use FortifyPlus\Fortify;
 
 return new class extends Migration
-{
+{ 
     /**
      * Run the migrations.
      */
@@ -17,6 +18,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+            if (Fortify::confirmsTwoFactorAuthentication()) {
+                $table->timestamp('two_factor_confirmed_at')->nullable();
+            }
             $table->rememberToken();
             $table->timestamps();
         });
